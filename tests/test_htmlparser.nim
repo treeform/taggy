3b7fc48f653544826944f0962c20ce47e246ae45
treeform <starplant@gmail.com>
2022-10-24 12:56:53 -0700

    mini test

--- tests/test_htmlparser.nim
+++ tests/test_htmlparser.nim
@@ -1,9 +1,11 @@
 import taggy/htmlparser
 
 let htmlFragments = @[
-  "<div><span><img></span></div>"
+  r"""<div></div>""",
+  r"""<div><span><img></span></div>""",
+  r"""<body><label for="duration">Duration</label><input type="range" name="duration"></body>"""
 ]
 
 for htmlFragment in htmlFragments:
-  # echo $parseHtml(htmlFragment)
+  echo $parseHtml(htmlFragment)
   doAssert $parseHtml(htmlFragment) == htmlFragment
